d03ab84511596c4a6d9bc73a088ddf2dae807d1a
Sahil Thakur <163773985+saolz@users.noreply.github.com>
2024-12-20 22:39:24 +0530

    Update firebase-config.ts

--- frontend/src/firebase-config.ts
+++ frontend/src/firebase-config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACsV5B2Ir5YF4u72WfiwGydhd4Ib5p0NE",
-  authDomain: "zsaa-b4dc5.firebaseapp.com",
-  projectId: "zsaa-b4dc5",
-  storageBucket: "zsaa-b4dc5.firebasestorage.app",
-  messagingSenderId: "759233918023",
-  appId: "1:759233918023:web:027404eef2525c3f312d62",
-  measurementId: "G-JBCQEZQFTS"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
